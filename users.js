17f13fa1abf5d95ebb364ddae0964191c361bb32
BlackkyDK <79807176+BlackkyDK@users.noreply.github.com>
2022-07-26 15:01:59 +0400

    автотесты

---
+++ users.js
@@ -0,0 +1,101 @@
+const User = require('../models/user');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => {
+      if (users.length === 0) {
+        res.status(400).send({ message: 'Переданы некорректные данные.' });
+        return;
+      }
+      res.status(200).send(users);
+    })
+    .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию.' }));
+};
+
+const getUser = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res
+          .status(404)
+          .send({ message: 'Пользователь по указанному _id не найден' });
+        return;
+      }
+      res.status(200).send(user);
+    })
+    .catch(() => res.status(500).send({ message: 'Ошибка по умолчанию.' }));
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  return User.create({ name, about, avatar })
+    .then((user) => {
+      res.status(201).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res
+          .status(400)
+          .send({ message: 'Переданы некорректные данные при создании пользователя' });
+      }
+      return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+    });
+};
+
+const updateUser = (req, res) => {
+  const { name, about } = req.body;
+  User.updateByIdUser(req.user._id, { name, about }, {
+    new: true,
+    runValidators: true,
+    upsert: false,
+  })
+    .then((user) => {
+      if (!user) {
+        res.status(404).send({ message: 'Пользователь не найден' });
+        return;
+      }
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+        return;
+      }
+      res.status(500).send({ message: 'Ошибка по умолчанию.' });
+    });
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.updateByIdAvatar(
+    req.user._id,
+    {
+      avatar,
+    },
+    {
+      new: true,
+      runValidators: true,
+      upsert: true,
+    },
+  )
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        return res
+          .status(400)
+          .send({ message: 'Переданы некорректные данные при обновлении аватара.' });
+      }
+
+      return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  updateAvatar,
+};
