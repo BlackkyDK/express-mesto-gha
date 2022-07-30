e716ec695f5815910c2ce4df94f4625f051c8d87
BlackkyDK <79807176+BlackkyDK@users.noreply.github.com>
2022-07-30 07:41:38 +0400

    автотесты

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const {
-  getUsers, getUser, createUser, updateUser, updateAvatar,
+  getUsers, getUserId, createUser, updateUser, updateAvatar,
 } = require('../controllers/users');
 
 router.get('/users', getUsers);
-router.get('/users/:userId', getUser);
+router.get('/users/:userId', getUserId);
 router.post('/users', createUser);
 router.patch('/users/me', updateUser);
 router.patch('/users/me/avatar', updateAvatar);
