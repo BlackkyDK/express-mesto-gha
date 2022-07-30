4753505c70c4d8dac2bbedfbac83a820d34fc8ef
BlackkyDK <79807176+BlackkyDK@users.noreply.github.com>
2022-07-30 07:21:42 +0400

    автотесты

--- routes/cards.js
+++ routes/cards.js
@@ -5,7 +5,7 @@ const {
 } = require('../controllers/cards');
 
 router.get('/cards', getCards);
-router.post('/cards ', createCard);
+router.post('/cards', createCard);
 router.delete('/cards/:cardId', deleteCard);
 router.put('/cards/:cardId/likes', likeCard);
 router.delete('/cards/:cardId/likes', dislikeCard);
