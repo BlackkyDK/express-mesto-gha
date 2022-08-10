fd697b3554409d44e9edbf6595a7ccefe0836af9
BlackkyDK <79807176+BlackkyDK@users.noreply.github.com>
2022-08-10 07:07:48 +0400

    автотесты

--- controllers/cards.js
+++ controllers/cards.js
@@ -12,7 +12,7 @@ const getCards = (req, res, next) => {
 const createCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(201).send(card))
+    .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequest('Переданы некорректные данные при создании карточки.'));
