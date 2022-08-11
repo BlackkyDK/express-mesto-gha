7d4149fa71532324d7c4eeb299f3160ded252483
BlackkyDK <79807176+BlackkyDK@users.noreply.github.com>
2022-08-12 00:35:38 +0400

    автотесты

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,14 +5,14 @@ const NotFound = require('../errors/NotFound');
 
 const getCards = (req, res, next) => {
   Card.find({})
-    .then((cards) => res.send({ data: cards }))
-    .catch((err) => next(err));
+    .then((cards) => res.send(cards))
+    .catch(next);
 };
 
 const createCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.send({ data: card }))
+    .then((card) => res.status(201).send(card))
     .catch((err) => {
       if (err.name === 'ValidationError') {
         next(new BadRequest('Переданы некорректные данные при создании карточки.'));
